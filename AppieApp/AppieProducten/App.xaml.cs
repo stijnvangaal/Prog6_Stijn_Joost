9acb42aa3e68441e2b1384aced5386894c4df576
Stijn van Gaal <stijn_50@msn.com>
2014-12-22 15:00:11 +0100

    Complete Set-up. Database created.  all 3 projects. double windows.

---
+++ AppieApp/AppieProducten/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace AppieProducten {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        void App_Startup(object sender, StartupEventArgs e) {
+            Boodschappen.MainWindow mainWindow = new Boodschappen.MainWindow();
+            mainWindow.Top = 100;
+            mainWindow.Left = 400;
+            mainWindow.Show();
+        }
+    }
+}
